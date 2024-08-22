2390440e2ea99d7f87199dc3bab335b7b2ef0a20
jpe7s <jpe7s.salt188@passfwd.com>
2024-08-22 11:23:59 -0500

    fix required module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,6 @@ module software.sava.anchor_src_gen {
   requires software.sava.core;
   requires software.sava.rpc;
   requires software.sava.solana_programs;
-  requires java.desktop;
 
   exports software.sava.anchor;
 }
