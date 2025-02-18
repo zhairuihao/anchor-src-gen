e1af95a22c62ef573f9bd6d02d44ecc4b1873a2e
ArikCCCCC <ruihao.zhai@gmail.com>
2025-02-18 15:18:35 +0800

    config

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,19 +1,11 @@
-module com.bumpin.trade.common {
-  exports com.bumpin.trade.common.anchor.types;
-  exports com.bumpin.trade.common.anchor;
-  exports com.bumpin.trade.common.dex.solana.anchor.types;
-  exports com.bumpin.trade.common.dex.solana.anchor;
-  exports com.bumpin.trade.common.pyth.rec.anchor.types;
-  exports com.bumpin.trade.common.pyth.rec.anchor;
-  exports com.bumpin.trade.common.shadow.storm.anchor.types;
-  exports com.bumpin.trade.common.shadow.storm.anchor;
-  exports com.bumpin.trade.common.wormhole.anchor.types;
-  exports com.bumpin.trade.common.wormhole.anchor;
-  exports software.sava.anchor;
-  requires java.base;
+module software.sava.anchor_src_gen {
   requires java.net.http;
-  requires software.sava.anchor_src_gen;
+
+  requires systems.comodal.json_iterator;
+
   requires software.sava.core;
   requires software.sava.rpc;
-  requires systems.comodal.json_iterator;
+
+  exports software.sava.anchor;
 }
+
